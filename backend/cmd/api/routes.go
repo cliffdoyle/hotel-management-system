1471ff27d0a3ffce12bda0a7a2d5b778dca2aa5f
cliffdoyle <oyoocliff471@gmail.com>
2025-08-16 14:23:28 +0300

    refactor(api):integrate helper methods into handlers and router Refactors the healthcheck handler and the httprouter configuration to use the new centralized error and JSON helper methods. This improves code reuse and standardizes responses across the application.

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -11,16 +11,15 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	// Convert our custom error handlers to http.HandlerFunc
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// A handler for catching panics and recovering.
 	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, i interface{}) {
 		app.logger.Error("recovered from panic", "panic", i)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 	}
-
-	// A handler for routes that are not found.
-	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-	})
 
 	// Our first endpoint: a health check.
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
